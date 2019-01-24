3a7518e136c7033eb0cf6c734baf9ca69fef7ebb
Natalia <mcb.nat@gmail.com>
2019-01-24 12:06:07 +0100

    Added routing

--- its-show-time-angular/src/app/app-routing.module.ts
+++ its-show-time-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NowPlayingComponent } from './components/now-playing/now-playing.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { UserMoviesComponent } from './components/user-movies/user-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', components: NowPlayingComponent },
+  { path: '/search=:query', components: SearchResultsComponent},
+  { path: '/user-movies', components: UserMoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
